a31bad88658812ad7d696c096b3885440805a7ba
Dinho Stork <dinhostork@live.com>
2023-04-25 22:20:07 -0300

    feat: adicionando funcionalidade que permite abertura e fechamento dos menus

---
+++ src/utils/componentsToggleMenu.ts
@@ -0,0 +1,37 @@
+import { useState, useEffect, useRef, useCallback } from "react";
+
+export const useComponentsMenu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = useCallback((value?: boolean) => {
+    setIsOpen((prevIsOpen) => {
+      if (value != null) {
+        return value;
+      }
+      return !prevIsOpen;
+    });
+  }, []);
+
+  const menuRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      console.log('outside isOpen: ', isOpen)
+      if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+        toggle(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [menuRef, toggle]);
+
+  return {
+    isOpen,
+    toggle,
+    menuRef,
+  };
+};

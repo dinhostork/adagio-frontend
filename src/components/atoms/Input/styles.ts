62db6ff2c57aa012037c9fa7269d52afefb5d4ff
Dinho Stork <dinhostork@live.com>
2023-04-27 16:40:49 -0300

    feat: caixa de busca

---
+++ src/components/atoms/Input/styles.ts
@@ -0,0 +1,24 @@
+import tw from "tailwind-styled-components";
+
+export const inputWrapper = `
+    relative
+`;
+
+export const iconWrapper = `
+    absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none text-gray-dark
+`;
+export const InputAtom = tw.input`
+        
+    placeholder-gray-light
+    h-12
+    bg-carbone-secondary
+    text-white
+    text-base
+    rounded-lg
+    block
+    w-72
+    pl-10
+    focus:outline-none
+    focus:ring-2
+
+`;
---
+++ src/components/atoms/Input/types.ts
@@ -0,0 +1,7 @@
+export interface InputTypes {
+  placeholder?: string;
+  type?: string;
+  id?: string;
+  icon?: React.ReactNode;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}

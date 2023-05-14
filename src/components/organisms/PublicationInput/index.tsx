aec75ed6d22e2ffe8e732231228e64f1ecbacff1
Dinho Stork <dinhostork@live.com>
2023-05-14 19:34:02 -0300

    feat: upload de midia

--- src/components/organisms/PublicationInput/index.tsx
+++ src/components/organisms/PublicationInput/index.tsx
@@ -4,11 +4,14 @@ import EmojiPicker from "emoji-picker-react";
 import { IoClose } from "react-icons/io5";
 import { GrEmoji } from "react-icons/gr";
 import { MdPermMedia } from "react-icons/md";
+import { TbFileUpload } from "react-icons/tb";
 
 export const PublicationInput = () => {
   const [showEmojiPicker, setShowEmojiPicker] = useState(false);
   const [showImageUploader, setShowImageUploader] = useState(false);
   const [text, setText] = useState("");
+  const [oppenedMediaArea, setoppenedMediaArea] = useState(false);
+  const [files, setFiles] = useState<File[]>([]);
 
   const handleEmojiClick = (emoji) => {
     const { emoji: emojiImage } = emoji;
@@ -26,6 +29,10 @@ export const PublicationInput = () => {
     );
   };
 
+  const handleMediaAreaIconClick = () => {
+    setoppenedMediaArea(!oppenedMediaArea);
+  };
+
   const inputRef = useRef();
   const emojiPickerRef = useRef();
 
@@ -41,6 +48,19 @@ export const PublicationInput = () => {
       document.removeEventListener("click", handleOutsideClick, true);
     };
   });
+
+  const handleFileSelect = (event: { target: { files: any } }) => {
+    const newFiles = [...event.target.files];
+    setFiles([...files, ...newFiles]);
+  };
+
+  const handleFileRemove = (index: number) => {
+    const newFiles = [...files];
+    if (index >= 0 && index < newFiles.length) {
+      newFiles.splice(index, 1);
+      setFiles(newFiles);
+    }
+  };
 
   return (
     <div className="flex flex-col justify-between w-full pr-4 pl-4 max-w-3xl relative">
@@ -68,8 +88,12 @@ export const PublicationInput = () => {
             onClick={() => setShowImageUploader(true)}
             className="ml-2 focus:outline-none"
           >
-            <span role="img" aria-label="image-button">
-              <MdPermMedia />
+            <span
+              role="img"
+              aria-label="image-button"
+              onClick={handleMediaAreaIconClick}
+            >
+              <MdPermMedia className={`${oppenedMediaArea ? "text-white" : "text-gray-light"}`}/>
             </span>
           </button>
         </div>
@@ -96,21 +120,62 @@ export const PublicationInput = () => {
         </div>
       )}
 
-      {showImageUploader && (
-        <div className="absolute bottom-10 right-2 z-10">
-          {/* Componente de upload de imagem */}
-          <button
-            onClick={() => setShowImageUploader(false)}
-            className="focus:outline-none"
-          >
-            Fechar
-          </button>
+      
+
+      {oppenedMediaArea && (
+        <div className="w-full h-24 bg-carbone rounded-md mt-4 flex items-center flex-row gap-4 overflow-scroll">
+          <div className="w-10 h-10 flex flex-row justify-center items-center border-2 border-gray-dark ml-8 transition ease-in-out">
+            <label htmlFor="file-upload" className="cursor-pointer">
+              <TbFileUpload className="text-gray-dark transition ease-in-out hover:text-white" size={24} />
+            </label>
+            <input
+              id="file-upload"
+              type="file"
+              className="hidden"
+              onChange={handleFileSelect}
+              multiple
+            />
+          </div>
+
+          {files.map((file, index) => {
+            const reader = new FileReader();
+            reader.readAsDataURL(file);
+            reader.onload = () => {
+              const previewUrl = reader.result;
+              setFiles((prevFiles) => {
+                const newFiles = [...prevFiles] as any;
+                if (newFiles[index]) {
+                  newFiles[index].previewUrl = previewUrl;
+                }
+                return newFiles;
+              });
+            };
+
+            return (
+              <div
+                key={index}
+                className="flex flex-row justify-between items-center w-20 h-20 border-2 border-gray-dark relative"
+              >
+                <img
+                  src={file.previewUrl}
+                  alt={file.name}
+                  className="w-full h-full object-cover"
+                />
+                <button
+                  onClick={() => handleFileRemove(index)}
+                  className="absolute right-0 top-0"
+                >
+                  <IoClose className="text-white" size={24} />
+                </button>
+              </div>
+            );
+          })}
         </div>
       )}
 
       {text.length > 0 && (
         <button
-          className="bg-blue-light rounded-md h-10 mt-4 disabled:bg-carbone transition ease-in-out hover:bg-blue-dark shadow-md"
+          className="bg-blue-light rounded-md h-10 mt-4 disabled:bg-carbone transition ease-in-out hover:bg-b shadow-md"
           disabled={text.length <= 0}
         >
           Publicar

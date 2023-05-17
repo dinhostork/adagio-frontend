bdcaafb46f12aaf676b949c396549a73ab6c0f26
Dinho Stork <dinhostork@live.com>
2023-05-16 22:41:48 -0300

    refactor: estilos para o arquivo styles

--- src/components/molecules/UserPost/index.tsx
+++ src/components/molecules/UserPost/index.tsx
@@ -3,9 +3,9 @@ import Image from "next/image";
 import { useEffect, useState } from "react";
 import { BiCommentEdit, BiLike, BiShare } from "react-icons/bi";
 import { IoIosMore } from "react-icons/io";
-import { register } from "swiper/element/bundle";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination, Autoplay } from "swiper";
+import { Navigation, Pagination } from "swiper";
+import * as styles from "./styles";
 
 // Import Swiper styles
 import "swiper/css";
@@ -89,29 +89,24 @@ export const UserPost = () => {
   return (
     <div className="overflow-y-auto">
       {posts.map((post: Post) => (
-        <div
-          className="flex flex-col justify-between w-full pr-4 pl-4 max-w-3xl relative mt-8"
-          key={post.id}
-        >
-          <div className="flex flex-col bg-carbone w-full rounded-md">
-            <div className="bg-night w-full flex-grow py-4 rounded-t-md px-10 flex flex-row items-center justify-between">
-              <div className="flex flex-row cursor-pointer">
+        <div className={styles.postWrapper} key={post.id}>
+          <div className={styles.postContainer}>
+            <div className={styles.postHeader}>
+              <div className={styles.ownerContainer}>
                 <AvatarImg
                   src={post.ownerPhoto}
                   alt={post.ownerName}
                   width={32}
                   height={32}
-                  className="rounded-full"
+                  className={styles.ownerPhoto}
                 />
-                <div className="flex flex-col ml-4 justify-center">
-                  <span className="text-white font-medium text-sm">
-                    {post.ownerName}
-                  </span>
-                  <span className="text-gray-light font-light text-xs">2h</span>
+                <div className={styles.ownerInfo}>
+                  <span className={styles.ownerName}>{post.ownerName}</span>
+                  <span className={styles.postTime}>2h</span>
                 </div>
               </div>
               <div
-                className="flex flex-row ml-auto items-center text-gray-dark hover:text-white transition ease-in-out cursor-pointer"
+                className={styles.postMenu}
                 onClick={() => {
                   // TODO - implement post menu
                 }}
@@ -119,32 +114,31 @@ export const UserPost = () => {
                 <IoIosMore />
               </div>
             </div>
-            <div className="flex flex-col w-full h-full p-4 px-10">
-              <span className="text-white font-light text-sm">
+            <div className={styles.postContent}>
+              <span className={styles.postText}>
                 {showFullText ? post.text : post.text.slice(0, 500)}
                 {post.text.length > 500 && (
                   <>
                     {showFullText ? (
                       <span
-                        className="text-blue-light font-light text-sm cursor-pointer"
+                        className={styles.viewMoreLess}
                         onClick={() => setShowFullText(false)}
                       >
-                        {" "}
                         Ver menos
                       </span>
                     ) : (
                       <span
-                        className="text-blue-light font-light text-sm cursor-pointer"
+                        className={styles.viewMoreLess}
                         onClick={() => setShowFullText(true)}
                       >
-                        {" ..."}Ver mais
+                        Ver mais...
                       </span>
                     )}
                   </>
                 )}
               </span>
             </div>
-            <div className="px-10 pb-4">
+            <div className={styles.mediaContainer}>
               {post.media.length > 0 && (
                 <Swiper
                   pagination={{
@@ -169,15 +163,14 @@ export const UserPost = () => {
                           src={media.url}
                           loop
                           controls
-                          className="w-full h-full self-center"
-                        
+                          className={styles.mediaVideo}
                         />
                       )}
                       {media.type === "audio" && (
                         <audio
                           src={media.url}
                           controls
-                          className="w-full h-full self-center"
+                          className={styles.mediaAudio}
                         />
                       )}
                     </SwiperSlide>
@@ -185,57 +178,47 @@ export const UserPost = () => {
                 </Swiper>
               )}
             </div>
-            <div className="flex flex-row w-full flex-grow py-6 rounded-b-md bg-night-light justify-between items-center px-10 bg-night ">
-              <div className="flex flex-row items-center justify-between w-full">
+            <div className={styles.postFooter}>
+              <div className={styles.footerButtons}>
                 <button
-                  className={`flex flex-row items-center w-auto font-light text-xs  ${
-                    post.liked ? "text-blue-light" : "text-white"
+                  className={`${styles.footerButton} ${
+                    post.liked ? styles.likedButton : ""
                   }`}
                   onClick={() => {
                     // TODO - implement like
                     post.liked = !post.liked;
                     post.liked ? post.likes++ : post.likes--;
-
                     setPosts([...posts]);
                   }}
                 >
-                  <BiLike size={16} className="text-xl mr-2" />
+                  <BiLike size={16} className={styles.likeIcon} />
                   Curtir
                   {post.likes > 0 && (
-                    <span className="font-light text-xs ml-2">
-                      {post.likes}
-                    </span>
+                    <span className={styles.likeCount}>{post.likes}</span>
                   )}
                 </button>
                 <button
-                  className="flex flex-row items-center w-auto font-light text-xs"
+                  className={styles.footerButton}
                   onClick={() => {
                     // TODO - implement comment modal
                   }}
                 >
-                  <BiCommentEdit
-                    size={16}
-                    className="text-white text-xl mr-2"
-                  />
+                  <BiCommentEdit size={16} className={styles.commentIcon} />
                   Comentar
                   {post.comments > 0 && (
-                    <span className="text-white font-light text-xs ml-2">
-                      {post.comments}
-                    </span>
+                    <span className={styles.commentCount}>{post.comments}</span>
                   )}
                 </button>
                 <button
-                  className="flex flex-row items-center w-auto font-light text-xs"
+                  className={styles.footerButton}
                   onClick={() => {
                     // TODO - implement share modal
                   }}
                 >
-                  <BiShare size={16} className="text-white text-xl mr-2" />
+                  <BiShare size={16} className={styles.shareIcon} />
                   Compartilhar
                   {post.shares > 0 && (
-                    <span className="text-white font-light text-xs ml-2">
-                      {post.shares}
-                    </span>
+                    <span className={styles.shareCount}>{post.shares}</span>
                   )}
                 </button>
               </div>

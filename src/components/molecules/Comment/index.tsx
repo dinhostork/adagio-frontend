a7b43d0fb446870529f9d4516aa8e341e49620ac
Dinho Stork <dinhostork@live.com>
2023-05-18 15:28:43 -0300

    refactor: estilo do comentário no arquivo styles

--- src/components/molecules/Comment/index.tsx
+++ src/components/molecules/Comment/index.tsx
@@ -1,32 +1,29 @@
 import { IoIosMore } from "react-icons/io";
-import { CommentType, CommentsProps } from "./types";
+import { CommentsProps } from "./types";
 import AvatarImg from "@/components/atoms/Avatar";
+import * as styles from "./styles";
 
 export const Comment: React.FC<CommentsProps> = ({ comment }) => {
   return (
-    <div className="flex flex-col w-full" key={comment.id}>
-      <div className="flex flex-row justify-between">
-        <div className="flex flex-row h-auto">
+    <div className={styles.commentContainer} key={comment.id}>
+      <div className={styles.loggedUserInformationsContainer}>
+        <div className={styles.avatarContainer}>
           <AvatarImg
             src={comment.ownerPhoto}
             alt={comment.ownerName}
             width={16}
             height={16}
-            className="rounded-full"
+            className={styles.commentOwnerPhoto}
           />
-          <div className="flex flex-col ml-2">
-            <div className="flex flex-row justify-between h-auto ">
+          <div className={styles.commentOwnerInformationsContainer}>
+            <div className={styles.ownerInfoContainer}>
               <div>
-                <span className="text-white font-normal text-sm cursor-pointer h-auto">
-                  {comment.ownerName}
-                </span>
-                <span className="text-gray-light fonte-normal text-sm ml-4">
-                  2h
-                </span>
+                <span className={styles.ownerName}>{comment.ownerName}</span>
+                <span className={styles.timeAgo}>2h</span>
               </div>
-              <div className="flex flex-row  h-auto right-2 bottom-1 gap-2">
+              <div className={styles.moreButtonContainer}>
                 <button
-                  className="text-gray-dark font-normal text-sm cursor-pointer h-auto hover:text-white transition-all ease-in-out"
+                  className={styles.moreButton}
                   onClick={() => {
                     // TODO - implement like comment
                   }}
@@ -35,10 +32,7 @@ export const Comment: React.FC<CommentsProps> = ({ comment }) => {
                 </button>
               </div>
             </div>
-            <span className="text-white font-light text-sm cursor-text h-auto mt-2">
-              {comment.text}
-            </span>
-            <div className="flex flex-row justify-start h-auto mt-2"></div>
+            <span className={styles.commentText}>{comment.text}</span>
           </div>
         </div>
       </div>
---
+++ src/components/molecules/Comment/styles.ts
@@ -0,0 +1,14 @@
+export const commentContainer = "flex flex-col w-full";
+export const avatarContainer = "flex flex-row h-auto";
+export const ownerInfoContainer = "flex flex-row justify-between h-auto";
+export const ownerName = "text-white font-normal text-sm cursor-pointer h-auto";
+export const timeAgo = "text-gray-light fonte-normal text-sm ml-4";
+export const moreButtonContainer =
+  "flex flex-row h-auto right-2 bottom-1 gap-2";
+export const moreButton =
+  "text-gray-dark font-normal text-sm cursor-pointer h-auto hover:text-white transition-all ease-in-out";
+export const commentText =
+  "text-white font-light text-sm cursor-text h-auto mt-1";
+export const commentOwnerPhoto = "rounded-full";
+export const commentOwnerInformationsContainer = "flex flex-col ml-2";
+export const loggedUserInformationsContainer = "flex flex-row justify-between";

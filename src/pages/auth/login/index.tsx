635817feb1f3b2a6a19bb5f67405dcb1ea12cff7
Dinho Stork <dinhostork@live.com>
2023-06-09 19:56:35 -0300

    fix: #34  Redirecionar para página principal caso o usuário já esteja autenticado

--- src/pages/auth/login/index.tsx
+++ src/pages/auth/login/index.tsx
@@ -2,8 +2,13 @@ import { LoginForm } from "@/components/organisms/LoginForm";
 import Head from "next/head";
 import Image from "next/image";
 import * as authStyles from "../../../styles/shared/authStyles";
+import {useSession} from "next-auth/react";
 
 export default function Login() {
+  const {data: session} = useSession();
+  if(session){
+    window.location.href = "/";
+  }
   return (
     <main className={authStyles.mainContainer}>
       <Head>
--- src/pages/auth/register/index.tsx
+++ src/pages/auth/register/index.tsx
@@ -2,8 +2,13 @@ import { RegisterForm } from "@/components/organisms/RegisterForm";
 import Head from "next/head";
 import Image from "next/image";
 import * as authStyles from '../../../styles/shared/authStyles'
+import {useSession} from "next-auth/react";
 
 export default function Register() {
+  const {data: session} = useSession();
+  if(session){
+    window.location.href = "/";
+  }
   return(
     <main className={authStyles.mainContainer}>
       <Head>
